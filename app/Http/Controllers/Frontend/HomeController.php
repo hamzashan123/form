06bde5428d57341088728e4e58117c7b341ca776
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2022-09-02 02:52:51 -0700

    updated

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return redirect()->route('admin.login');
+        return url('/admin');
         $coupon = Coupon::active()->public()->first();
 
         $categories = Category::select('slug', 'cover', 'name')
