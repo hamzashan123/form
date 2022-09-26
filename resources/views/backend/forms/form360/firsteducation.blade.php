78ac049a470da2089d98ad858fb16652453a36f8
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-09-26 16:56:58 +0500

    First Education Screen completed

--- resources/views/backend/forms/form360/firsteducation.blade.php
+++ resources/views/backend/forms/form360/firsteducation.blade.php
@@ -1,68 +1,255 @@
 <fieldset id="fieldsetthree">
-                            <div class="form-card">
-                                <div class="row">
-                                    <div class="col-7">
-                                        <h2 class="fs-title">WORK EXPERIENCE INFO:</h2>
-                                    </div>
-                                    <div class="col-5">
-                                        <h2 class="steps">Step 3 - 4</h2>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-12 pl-0 mt-20">
-                                    <h3 class="fs-title">WORK EXPERIENCE - IF PERFORMED IN AUSTRALIA:</h3>
-                                    <p>PLEASE REFER TO THE LAST 10 YEARS AND ONLY INDICATE RELEVANT WORK EXPERIENCE IN LINE WITH
-                                        THE OCCUPATION </p>
-                                </div>
+    <div class="form-card">
+        <div class="row">
+            <div class="col-7">
+                <h2 class="fs-title">FIRST EDUCATION</h2>
+            </div>
+            <div class="col-5">
+                <h2 class="steps">Step 3 - 14</h2>
+            </div>
+        </div>
 
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td> Employer - Business name 
-                                            </td>
-                                            <td> <input type="text" name="work_business_name" id="work_business_name" @if(isset($data['work'][0]->work_business_name)) value="{{$data['work'][0]->work_business_name}}" @endif /> </td>
-                                        </tr>
+        <h2 class="fs-title">EDUCATION HISTORY – INFO
+        </h2>
+        <p>Indicate what is your highest recognised qualification obtained </p>
 
-                                        <tr>
-                                            <td> Full address
-                                            </td>
-                                            <td> <input type="text" name="work_fulladdress" id="work_fulladdress" @if(isset($data['work'][0]->work_fulladdress)) value="{{$data['work'][0]->work_fulladdress}}" @endif/> </td>
-                                        </tr>
 
-                                        <tr>
-                                            <td> Sector of activity </td>
-                                            <td> <input type="text" name="work_activity" id="work_activity" @if(isset($data['work'][0]->work_activity)) value="{{$data['work'][0]->work_activity}}" @endif/>
-                                            </td>
-                                        </tr>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        Indicate what is your highest recognised
+                        qualification obtained
 
-                                        <tr>
-                                            <td> Your role and position </td>
-                                            <td> <input type="text" name="work_role" id="work_role" @if(isset($data['work'][0]->work_role)) value="{{$data['work'][0]->work_role}}" @endif/>
-                                            </td>
-                                        </tr>
+                    </td>
 
-                                      
-                                
-                                        <tr>
-                                            <td> Phone contact of the referee 
-                                            </td>
-                                            <td> <input type="text" name="work_referee_phone"
-                                                    id="work_referee_phone" class="numberfixed" @if(isset($data['work'][0]->work_referee_phone)) value="{{$data['work'][0]->work_referee_phone}}" @endif />
-                                            </td>
-                                        </tr>
+                    <td>
+                        <select name="first_education_qualification_obtained" id="first_education_qualification_obtained" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="phd"> PHD </option>
+                            <option value="master"> Masters Degree </option>
+                            <option value="bachelor"> Bachelor Degree </option>
+                            <option value="vocational"> Vocational diploma </option>
+                            <option value="highschool"> High School diploma </option>
+                            <option value="other"> Other </option>
 
-                                    </tbody>
-                                </table>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
 
-        
-                                    
-                            </div> 
-                                
-                                  
-                                     
-                                   
-                                   
-                            <input type="button" name="next" class="next action-button" value="Next" /> 
-                            <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
-                                    
-                        </fieldset>
+        <h2 class="fs-title">EDUCATION BACKGROUND
+        </h2>
+        <p>PRIMARY SCHOOL or equivalent </p>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Name Of the School </td>
+                    <td> <input type="text" name="firstedu_background_nameofschool" id="firstedu_background_nameofschool" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td> Full address
+                    </td>
+                    <td> <input type="text" name="firstedu_background_fulladdress" id="firstedu_background_fulladdress" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td> Date course started
+
+                    </td>
+                    <td> <input type="date" name="firstedu_background_datecoursesstarted" id="firstedu_background_datecoursesstarted" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Date course ended
+
+                    </td>
+                    <td> <input type="date" name="firstedu_background_datecoursesended" id="firstedu_background_datecoursesended" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Year obtained
+
+                    </td>
+                    <td> <input type="date" name="firstedu_background_yearobt" id="firstedu_background_yearobt" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Legal duration of the course
+
+
+                    </td>
+                    <td> <input type="date" name="firstedu_background_legal_duration" id="firstedu_background_legal_duration" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Qualification obtained
+
+
+
+                    </td>
+                    <td> <input type="date" name="firstedu_background_qualification_obtained" id="firstedu_background_qualification_obtained" />
+                    </td>
+                </tr>
+
+
+            </tbody>
+        </table>
+
+        <h2 class="fs-title">MIDDLE SCHOOL or equivalent</h2>
+
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Name Of the School </td>
+                    <td> <input type="text" name="firstedu_middle_nameofschool" id="firstedu_middle_nameofschool" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td> Full address
+                    </td>
+                    <td> <input type="text" name="firstedu_middle_fulladdress" id="firstedu_middle_fulladdress" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td> Date course started
+
+                    </td>
+                    <td> <input type="date" name="firstedu_middle_datecoursesstarted" id="firstedu_middle_datecoursesstarted" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Date course ended
+
+                    </td>
+                    <td> <input type="date" name="firstedu_middle_datecoursesended" id="firstedu_middle_datecoursesended" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Year obtained
+
+                    </td>
+                    <td> <input type="date" name="firstedu_middle_yearobt" id="firstedu_middle_yearobt" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Legal duration of the course
+
+
+                    </td>
+                    <td> <input type="date" name="firstedu_middle_legal_duration" id="firstedu_middle_legal_duration" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Qualification obtained
+
+
+
+                    </td>
+                    <td> <input type="date" name="firstedu_middle_qualification_obtained" id="firstedu_middle_qualification_obtained" />
+                    </td>
+                </tr>
+
+
+            </tbody>
+        </table>
+
+
+        <h2 class="fs-title">HIGH SCHOOL or equivalent
+        </h2>
+
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Name Of the School </td>
+                    <td> <input type="text" name="firstedu_high_nameofschool" id="firstedu_high_nameofschool" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td> Full address
+                    </td>
+                    <td> <input type="text" name="firstedu_high_fulladdress" id="firstedu_high_fulladdress" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td> Date course started
+
+                    </td>
+                    <td> <input type="date" name="firstedu_high_datecoursesstarted" id="firstedu_high_datecoursesstarted" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Date course ended
+
+                    </td>
+                    <td> <input type="date" name="firstedu_high_datecoursesended" id="firstedu_high_datecoursesended" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Year obtained
+
+                    </td>
+                    <td> <input type="date" name="firstedu_high_yearobt" id="firstedu_high_yearobt" />
+                    </td>high
+                </tr>
+
+                <tr>
+                    <td> Legal duration of the course
+
+
+                    </td>
+                    <td> <input type="date" name="firstedu_high_legal_duration" id="firstedu_high_legal_duration" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Qualification obtained
+
+
+
+                    </td>
+                    <td> <input type="date" name="firstedu_high_qualification_obtained" id="firstedu_high_qualification_obtained" />
+                    </td>
+                </tr>
+
+
+            </tbody>
+        </table>
+
+    </div>
+
+
+
+
+
+    <input type="button" name="next" class="next action-button" value="Next" />
+    <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
+
+</fieldset>
