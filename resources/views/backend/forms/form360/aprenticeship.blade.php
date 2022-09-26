1100b1cd93c2a6377412772e051b33ce39129c68
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-09-26 19:35:36 +0500

    Apprenticeship completed

--- resources/views/backend/forms/form360/aprenticeship.blade.php
+++ resources/views/backend/forms/form360/aprenticeship.blade.php
@@ -1,65 +1,264 @@
 <fieldset id="fieldsettwo">
-                                <div class="form-card">
-                                    <div class="row">
-                                        <div class="col-7">
-                                            <h2 class="fs-title">EDUCATION BACKGROUND - INFO:</h2>
-                                        </div>
-                                        <div class="col-5">
-                                            <h2 class="steps">Step 2 - 4</h2>
-                                        </div>
-                                    </div> 
-                                    
-                                   
+    <div class="form-card">
+        <div class="row">
+            <div class="col-7">
+                <h2 class="fs-title">APRENTICESHIP (if applicable)</h2>
+            </div>
+            <div class="col-5">
+                <h2 class="steps">Step 5 - 14</h2>
+            </div>
+        </div>
 
 
-                                    <table>
-                                        <tbody>
-                                            <tr>
-                                                <td> Full name of the school
-                                                </td>
-                                                <td> <input type="text" name="edu_info_junior_fullname" id="edu_info_junior_fullname"  @if(isset($data['education'][0]->edu_info_junior_fullname)) value="{{$data['education'][0]->edu_info_junior_fullname}}" @endif /> </td>
-                                            </tr>
 
-                                            <tr>
-                                                <td> Full address
-                                                </td>
-                                                <td> <input type="text" name="edu_info_junior_fulladdress" id="edu_info_junior_fulladdress" @if(isset($data['education'][0]->edu_info_junior_fulladdress)) value="{{$data['education'][0]->edu_info_junior_fulladdress}}" @endif/> </td>
-                                            </tr>
 
-                                            <tr>
-                                                <td> Year obtained </td>
-                                                <td> <input type="date" name="edu_info_junior_yearobtainted" id="edu_info_junior_yearobtainted" @if(isset($data['education'][0]->edu_info_junior_yearobtainted)) value="{{$data['education'][0]->edu_info_junior_yearobtainted}}" @endif/>
-                                                </td>
-                                            </tr>
+        <table>
+            <tbody>
+                <tr>
+                    <td> Did you undergo a period of apprenticeship (apprendistato) either during your formal studies or after your finished you course of study?
+                    </td>
+                    <td> <input type="text" name="apprenticeship_formal_studies" id="apprenticeship_formal_studies" /> </td>
+                </tr>
 
-                                            <tr>
-                                                <td> Legal duration of the course </td>
-                                                <td> <input type="text" name="edu_info_junior_course" id="edu_info_junior_course" @if(isset($data['education'][0]->edu_info_junior_course)) value="{{$data['education'][0]->edu_info_junior_course}}" @endif/>
-                                                </td>
-                                            </tr>
+                <tr>
+                    <td> If yes, please specify what type of apprenticeship you did</td>
+                    <td> <input type="text" name="apprenticeship_formal_studies_specify" id="apprenticeship_formal_studies_specify" /> </td>
+                </tr>
 
-                                            <tr>
-                                                <td> Indicate the START date of your studies (DD-MMYYY)
-                                                </td>
-                                                <td> <input type="date" name="edu_info_junior_startdate" id="edu_info_junior_startdate" @if(isset($data['education'][0]->edu_info_junior_startdate)) value="{{$data['education'][0]->edu_info_junior_startdate}}" @endif/> </td>
-                                            </tr>
+                <tr>
+                    <td> Indicate the total duration of the apprenticeship </td>
+                    <td> <input type="text" name="apprenticeship_total_duration" id="apprenticeship_total_duration" /> </td>
+                </tr>
 
-                                            <tr>
-                                                <td> Indicate the END date of your studies (DD-MMYYY)
-                                                </td>
-                                                <td> <input type="date" name="edu_info_junior_enddate"
-                                                        id="edu_info_junior_enddate" @if(isset($data['education'][0]->edu_info_junior_enddate)) value="{{$data['education'][0]->edu_info_junior_enddate}}" @endif/>
-                                                </td>
-                                            </tr>
+                <tr>
+                    <td> Was the apprenticeship completed? </td>
+                    <td> <input type="text" name="apprenticeship_completed" id="apprenticeship_completed" /> </td>
+                </tr>
 
-                                        </tbody>
-                                    </table>
 
-                                  
-                                
-                                
-                               
-                                    </div>
-                                <input type="button" name="next" class="next action-button" value="Next" /> 
-                                <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
-                            </fieldset>
+                <tr>
+                    <td>What was the qualification you obtained at the
+                        conclusion of the apprenticeship? </td>
+                    <td> <input type="text" name="apprenticeship_conclusion" id="apprenticeship_conclusion" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Name of the company you did your apprenticeship
+                        with </td>
+                    <td> <input type="text" name="apprenticeship_company" id="apprenticeship_company" /> </td>
+                </tr>
+
+                <tr>
+                    <td> Address of the company </td>
+                    <td> <input type="text" name="apprenticeship_company_address" id="apprenticeship_company_address" /> </td>
+                </tr>
+
+                <tr>
+                    <td> Did you need a visa to perform the work in this country? If so, please specify the visa you held </td>
+                    <td> <input type="text" name="apprenticeship_visa_country" id="apprenticeship_visa_country" /> </td>
+                </tr>
+
+                <tr>
+                    <td> Your position </td>
+                    <td> <input type="text" name="apprenticeship_position" id="apprenticeship_position" /> </td>
+                </tr>
+
+
+                <tr>
+                    <td> Start date </td>
+                    <td> <input type="date" name="apprenticeship_start_date" id="apprenticeship_start_date" /> </td>
+                </tr>
+
+                <tr>
+                    <td> End date (if still working indicate date of today) </td>
+                    <td> <input type="date" name="apprenticeship_end_date" id="apprenticeship_end_date" /> </td>
+                </tr>
+
+
+                <tr>
+                    <td>Was this work full-time or part-time? </td>
+                    <td> <input type="text" name="apprenticeship_fulltime_parttime" id="apprenticeship_fulltime_parttime" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Indicate the number of hours per week performed </td>
+                    <td> <input type="text" name="apprenticeship_week_perhour" id="apprenticeship_week_perhour" /> </td>
+                </tr>
+
+                <tr>
+                    <td>List your main duties and responsibilities performed (please indicate 5 main duties and responsibilities) </td>
+                    <td> <input type="text" name="apprenticeship_duties_responsibities" id="apprenticeship_duties_responsibities" /> </td>
+                </tr>
+
+
+                <tr>
+                    <td>Did you complete the apprenticeship and obtain the formal qualification through the apprenticeship?</td>
+                    <td> <input type="text" name="apprenticeship_formal_qualification" id="apprenticeship_formal_qualification" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Did you receive payments for this apprenticeship period?</td>
+                    <td>
+                        <select name="apprenticeship_payments" id="apprenticeship_payments" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Can this work period be documented? </td>
+                    <td>
+                        <select name="apprenticeship_period" id="apprenticeship_period" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Was this work performed as an employee or by issuing an invoice?</td>
+                    <td> <input type="text" name="apprenticeship_issue_invoice" id="apprenticeship_issue_invoice" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Are you able to provide an employment or collaboration contract for this work period </td>
+                    <td>
+                        <select name="apprenticeship_employment_collaboration" id="apprenticeship_employment_collaboration" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Can you provide payslips for this work period </td>
+                    <td>
+                        <select name="apprenticeship_payslips" id="apprenticeship_payslips" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Can you provide taxation documents for this period such as Income tax returns?
+                    </td>
+                    <td>
+                        <select name="apprenticeship_income" id="apprenticeship_income" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Can you provide bank statement showing payment in
+                        your favour for the work performed?
+                    </td>
+                    <td>
+                        <select name="apprenticeship_bank_statement" id="apprenticeship_bank_statement" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Are you able to get a reference letter from your
+                        employer?
+                    </td>
+                    <td>
+                        <select name="apprenticeship_reference_letter" id="apprenticeship_reference_letter" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Name and surname of your referee</td>
+                    <td> <input type="text" name="apprenticeship_name_surname" id="apprenticeship_name_surname" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Contact number of your referee</td>
+                    <td> <input type="tel" name="apprenticeship_referee_contact" id="apprenticeship_referee_contact" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Contact number of your referee</td>
+                    <td> <input type="tel" name="apprenticeship_referee_contact" id="apprenticeship_referee_contact" /> </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;">DOCS for Apprenticeship
+        </h2>
+        <p style="color:red">Attachments must be colour scans of the original document and uploaded in PDF format only. Please consolidate all
+            multipage files into one PDF document for each attachment type. Maximum PDF file size is 3MB per PDF.
+            Unnecessary additional documents will cause delays in processing. </p>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Apprenticeship contract
+                    </td>
+                    <td> <input type="file" name="apprenticeship_contract_doc" id="apprenticeship_contract_doc"></td>
+                </tr>
+
+                <tr>
+                    <td> Apprenticeship completion letter issued by the employer
+                    </td>
+                    <td> <input type="file" name="apprenticeship_completion_letter" id="apprenticeship_completion_letter"></td>
+                </tr>
+
+                <tr>
+                    <td> Provide 1 payslip for your current or most recent
+                        apprenticeship experience here (current year)
+                    </td>
+                    <td> <input type="file" name="apprenticeship_payslip_current_year" id="apprenticeship_payslip_current_year"></td>
+                </tr>
+
+                <tr>
+                    <td> Provide 1 payslip for the previous year of
+                        apprenticeship
+                    </td>
+                    <td> <input type="file" name="apprenticeship_previous_year" id="apprenticeship_previous_year"></td>
+                </tr>
+
+                <tr>
+                    <td> Income tax return of the most the current or most
+                        recent year (in Italy CUD)
+                    </td>
+                    <td> <input type="file" name="apprenticeship_tax_return" id="apprenticeship_tax_return"></td>
+                </tr>
+
+                <tr>
+                    <td> Social Security payment evidence chart (such as
+                        Tabella INPS or any other equivalent document issued
+                        by the National Social Security Body or dedicated
+                        registration body)
+                    </td>
+                    <td> <input type="file" name="apprenticeship_social_security" id="apprenticeship_social_security"></td>
+                </tr>
+
+
+            </tbody>
+        </table>
+
+
+
+    </div>
+    <input type="button" name="next" class="next action-button" value="Next" />
+    <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
+</fieldset>
