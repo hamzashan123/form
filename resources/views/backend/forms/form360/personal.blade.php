10dff6cbacf6a99d0754e13ac2ed28d4f1fdac16
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-09-26 16:40:50 +0500

    Personal Complete

--- resources/views/backend/forms/form360/personal.blade.php
+++ resources/views/backend/forms/form360/personal.blade.php
@@ -245,15 +245,207 @@
                     <td> <input type="text" name="personal_legal_action_specifytaken" id="personal_legal_action_specifytaken" />
                     </td>
                 </tr>
+            </tbody>
+        </table>
 
 
+        <h2 class="fs-title" style="margin-top: 10px;">DOCS FOR PERSONAL DETAILS</h2>
+        <p style="color:red"> Attachments must be colour scans of the original document and uploaded in PDF format only. Please consolidate all
+            multipage files into one PDF document for each attachment type. Maximum PDF file size is 3MB per PDF.
+            Unnecessary additional documents will cause delays in processing. </p>
 
+        <table>
+            <tbody>
 
+                <tr>
+                    <td> Personal Biodata Page </td>
+                    <td> <input type="file" name="personal_passport_biodata_page" id="personal_passport_biodata_page"></td>
+                </tr>
+
+                <tr>
+                    <td> ID card or driving license </td>
+                    <td> <input type="file" name="personal_id_card_driving_license" id="personal_id_card_driving_license"></td>
+                </tr>
+
+                <tr>
+                    <td> Updated CV/Resume </td>
+                    <td> <input type="file" name="personal_update_resume" id="personal_update_resume"></td>
+                </tr>
 
 
 
             </tbody>
         </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;"> SKILLS ASSESSMENT </h2>
+        <table>
+            <tbody>
+
+                <tr>
+                    <td>Do you hold a skills assessment? If so, in what occupation (please attach outcome letter) </td>
+                    <td>
+                        <select name="personal_skills_assessment" id="personal_skills_assessment" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>If yes, please indicate in what occupation you obtained the assessment </td>
+                    <td> <input type="text" name="personal_skills_assessment_specifytaken" id="personal_health_examination_specifytaken" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If you do not have a skills assessment, have you lodged a skills assessment application that is still pending? </td>
+                    <td>
+                        <select name="personal_skills_assessment_application" id="personal_skills_assessment_application" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>If yes, please specify what skills assessment has been lodged and for what occupation
+                    </td>
+                    <td> <input type="text" name="personal_skills_assessment_application_specifytaken" id="personal_skills_assessment_application_specifytaken" />
+                    </td>
+                </tr>
+            </tbody>
+
+
+        </table>
+
+
+        <h2 class="fs-title" style="margin-top: 10px;">SKILLS ASSESSMENT - DOCS</h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Skills assessment outcome letter </td>
+                    <td> <input type="file" name="personal_skills_assessment_outcome_letter" id="personal_skills_assessment_outcome_letter"></td>
+                </tr>
+            </tbody>
+        </table>
+
+
+        <h2 class="fs-title" style="margin-top: 10px;">LANGUAGES</h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> What is your main language </td>
+                    <td> <input type="text" name="personal_main_languages" id="personal_main_languages" /></td>
+                </tr>
+
+                <tr>
+                    <td>Have you ever taken a formal English test such
+                        as IELTS Test or PTE, TOEFL; CAE Advanced, OET,
+                        in the last 36 months? </td>
+                    <td>
+                        <select name="personal_languages_english_test" id="personal_languages_english_test" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If yes, please specify what Test was taken </td>
+                    <td>
+                        <select name="personal_englishtest_specify" id="personal_englishtest_specify" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="ielts"> IELTS </option>
+                            <option value="pte"> PTE Academic </option>
+                            <option value="toefl"> TOEFL </option>
+                            <option value="oet"> OET </option>
+                            <option value="cae"> CAE Advanced </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Date the English test was taken </td>
+                    <td> <input type="date" name="personal_englishtest_taken" id="personal_englishtest_taken" /></td>
+                </tr>
+
+                <tr>
+                    <td>Have you studied in a secondary and or tertiary
+                        institution where the instruction was in English?
+                    </td>
+                    <td>
+                        <select name="personal_tertiary_institution" id="personal_tertiary_institution" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> If yes, please specify what course level it was,
+                        the duration and where the course was taken </td>
+                    <td> <input type="date" name="personal_course_duration" id="personal_course_duration" /></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;">LANGUAGES - DOCS</h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> English test to be uploaded here </td>
+                    <td> <input type="file" name="personal_languages_english_test" id="personal_languages_english_test"></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;">LICENCE AND REGSITRATION</h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        Is registration or a licence required to work in
+                        the nominated occupation in the location where
+                        you will be working in Australia or New
+                        Zealand
+                    </td>
+                    <td>
+                        <select name="personal_license_and_registration" id="personal_license_and_registration" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                            <option value="other"> OTHER </option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;">LICENCE AND REGISTRATION – DOCS
+        </h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Licence/Registration evidence </td>
+                    <td> <input type="file" name="personal_license_and_registration" id="personal_license_and_registration"></td>
+                </tr>
+            </tbody>
+        </table>
+
+
+
+
     </div>
     <input type="button" name="next" class="next action-button" value="Next" />
 </fieldset>
