00f1e66b2744c5c3612ee798d3c739a01034475f
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-09-26 16:07:44 +0500

    Personal main applicant

--- resources/views/backend/forms/form360/personal.blade.php
+++ resources/views/backend/forms/form360/personal.blade.php
@@ -1,65 +1,259 @@
-<fieldset id="fieldsettwo">
-                                <div class="form-card">
-                                    <div class="row">
-                                        <div class="col-7">
-                                            <h2 class="fs-title">EDUCATION BACKGROUND - INFO:</h2>
-                                        </div>
-                                        <div class="col-5">
-                                            <h2 class="steps">Step 2 - 4</h2>
-                                        </div>
-                                    </div> 
-                                    
-                                   
+<fieldset id="fieldsetone">
+    <div class="form-card">
+        <div class="row">
+            <div class="col-7">
+                <h2 class="fs-title">Personal:</h2>
+            </div>
+            <div class="col-5">
+                <h2 class="steps">Step 2 - 14 </h2>
+            </div>
+        </div>
+
+        <h2 class="fs-title">MAIN APPLICANT</h2>
+        <table>
+            <tbody>
+                <tr>
+                    <td> Name </td>
+                    <td> <input type="text" name="personal_name" id="personal_name" />
+                    </td>
+                </tr>
 
 
-                                    <table>
-                                        <tbody>
-                                            <tr>
-                                                <td> Full name of the school
-                                                </td>
-                                                <td> <input type="text" name="edu_info_junior_fullname" id="edu_info_junior_fullname"  @if(isset($data['education'][0]->edu_info_junior_fullname)) value="{{$data['education'][0]->edu_info_junior_fullname}}" @endif /> </td>
-                                            </tr>
+                <tr>
+                    <td> Surname </td>
+                    <td> <input type="text" name="personal_surname" id="personal_surname" />
+                    </td>
+                </tr>
 
-                                            <tr>
-                                                <td> Full address
-                                                </td>
-                                                <td> <input type="text" name="edu_info_junior_fulladdress" id="edu_info_junior_fulladdress" @if(isset($data['education'][0]->edu_info_junior_fulladdress)) value="{{$data['education'][0]->edu_info_junior_fulladdress}}" @endif/> </td>
-                                            </tr>
 
-                                            <tr>
-                                                <td> Year obtained </td>
-                                                <td> <input type="date" name="edu_info_junior_yearobtainted" id="edu_info_junior_yearobtainted" @if(isset($data['education'][0]->edu_info_junior_yearobtainted)) value="{{$data['education'][0]->edu_info_junior_yearobtainted}}" @endif/>
-                                                </td>
-                                            </tr>
+                <tr>
+                    <td> Are or have you ever been formally known by
+                        any other names or surnames?
+                    </td>
+                    <td> <input type="text" name="personal_name_surname" id="personal_name_surname" />
+                    </td>
+                </tr>
 
-                                            <tr>
-                                                <td> Legal duration of the course </td>
-                                                <td> <input type="text" name="edu_info_junior_course" id="edu_info_junior_course" @if(isset($data['education'][0]->edu_info_junior_course)) value="{{$data['education'][0]->edu_info_junior_course}}" @endif/>
-                                                </td>
-                                            </tr>
 
-                                            <tr>
-                                                <td> Indicate the START date of your studies (DD-MMYYY)
-                                                </td>
-                                                <td> <input type="date" name="edu_info_junior_startdate" id="edu_info_junior_startdate" @if(isset($data['education'][0]->edu_info_junior_startdate)) value="{{$data['education'][0]->edu_info_junior_startdate}}" @endif/> </td>
-                                            </tr>
+                <tr>
+                    <td> Civil status </td>
+                    <td>
+                        <select name="personal_civil_status" id="personal_civil_status" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="single"> Single </option>
+                            <option value="married"> Married </option>
+                            <option value="formalcertificate"> De facto relationship with formal certificate
+                                issued </option>
+                            <option value="formalcertificateissued"> De facto relationship without a formal
+                                certificate issued </option>
+                            <option value="separated"> Separated </option>
+                            <option value="sivorced"> Divorced </option>
+                            <option value="widowed"> Widowed </option>
 
-                                            <tr>
-                                                <td> Indicate the END date of your studies (DD-MMYYY)
-                                                </td>
-                                                <td> <input type="date" name="edu_info_junior_enddate"
-                                                        id="edu_info_junior_enddate" @if(isset($data['education'][0]->edu_info_junior_enddate)) value="{{$data['education'][0]->edu_info_junior_enddate}}" @endif/>
-                                                </td>
-                                            </tr>
+                        </select>
+                    </td>
+                </tr>
 
-                                        </tbody>
-                                    </table>
 
-                                  
-                                
-                                
-                               
-                                    </div>
-                                <input type="button" name="next" class="next action-button" value="Next" /> 
-                                <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
-                            </fieldset>
+                <tr>
+                    <td> Date of birth
+
+                    </td>
+                    <td> <input type="date" name="personal_dob" id="personal_dob" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Place of birth
+
+                    </td>
+                    <td> <input type="text" name="personal_pob" id="personal_pob" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Gender
+
+                    </td>
+                    <td> <input type="text" name="personal_gender" id="personal_gender" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Nationality
+
+                    </td>
+                    <td> <input type="text" name="personal_nationality" id="personal_nationality" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Other Nationality (If applicable)
+
+                    </td>
+                    <td> <input type="text" name="personal_other_nationality" id="personal_other_nationality" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Country of residence
+
+                    </td>
+                    <td> <input type="text" name="personal_countryofresidence" id="personal_countryofresidence" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Address where you live in your usual country of
+                        residence
+
+                    </td>
+                    <td> <input type="text" name="personal_addressusualcountry" id="personal_addressusualcountry" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Address in Australia or New Zealand (if
+                        applicable)
+
+
+                    </td>
+                    <td> <input type="text" name="personal_addressinausornz" id="personal_addressinausornz" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Contact number in your usually country of
+                        residence</td>
+                    <td> <input type="tel" name="personal_contactnumberusuallycountry" id="personal_contactnumberusuallycountry" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Whatsapp contact number
+                    </td>
+                    <td> <input type="tel" name="personal_whatsappcontact" id="personal_whatsappcontact" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Phone number in Australia or New Zealand (if
+                        already available)
+                    </td>
+                    <td> <input type="tel" name="personal_contactnumberausornz" id="personal_contactnumberausornz" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Email address</td>
+                    <td> <input type="email" name="personal_emailaddress" id="personal_emailaddress" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td> Are you currently in Australia or New Zealand? </td>
+                    <td>
+                        <select name="personal_aus_or_nz" id="personal_aus_or_nz" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> If in Australia or New Zealand, what visa do you hold? </td>
+                    <td> <input type="text" name="personal_visahold" id="personal_visahold" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If in Australia or New Zealand, when does your visa
+                        expire </td>
+                    <td> <input type="date" name="personal_visaexpire" id="personal_visaexpire" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Have you undertaken a health examination for an Australia or New Zealandn visa in the last 12 months? </td>
+                    <td>
+                        <select name="personal_health_examination" id="personal_health_examination" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>If yes, please specify when it was taken </td>
+                    <td> <input type="date" name="personal_health_examination_specifytaken" id="personal_health_examination_specifytaken" />
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>During your stay in Australia or New Zealand, are you planning to enter a hospital or a health care facility (including nursing homes) </td>
+                    <td>
+                        <select name="personal_health_care" id="personal_health_care" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If yes, please specify </td>
+                    <td> <input type="text" name="personal_health_care_specifytaken" id="personal_health_care_specifytaken" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Have you ever been convicted of an offence in any country (including any conviction which is now removed from official records)? </td>
+                    <td>
+                        <select name="personal_conviction" id="personal_conviction" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If yes, please specify </td>
+                    <td> <input type="text" name="personal_conviction_specifytaken" id="personal_conviction_specifytaken" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Have you ever been charged with any offence that is currently awaiting legal action? </td>
+                    <td>
+                        <select name="personal_legal_action" id="personal_legal_action" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If yes, please specify </td>
+                    <td> <input type="text" name="personal_legal_action_specifytaken" id="personal_legal_action_specifytaken" />
+                    </td>
+                </tr>
+
+
+
+
+
+
+
+            </tbody>
+        </table>
+    </div>
+    <input type="button" name="next" class="next action-button" value="Next" />
+</fieldset>
