544706581030277235910997600afa54b2b3b891
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-09-26 20:08:51 +0500

    Work experience done

--- resources/views/backend/forms/form360/workexperience.blade.php
+++ resources/views/backend/forms/form360/workexperience.blade.php
@@ -1,65 +1,343 @@
-<fieldset id="fieldsettwo">
-                                <div class="form-card">
-                                    <div class="row">
-                                        <div class="col-7">
-                                            <h2 class="fs-title">EDUCATION BACKGROUND - INFO:</h2>
-                                        </div>
-                                        <div class="col-5">
-                                            <h2 class="steps">Step 2 - 4</h2>
-                                        </div>
-                                    </div> 
-                                    
-                                   
+<fieldset id="fieldsetsix">
+    <div class="form-card">
+        <div class="row">
+            <div class="col-7">
+                <h2 class="fs-title">- WORK EXPERIENCE (CURRENT OR MOST RECENT) NOT OLDER THAN 10 YEARS:</h2>
+            </div>
+            <div class="col-5">
+                <h2 class="steps">Step 6 - 14</h2>
+            </div>
+        </div>
+
+        <table>
+            <tbody>
+
+                <tr>
+                    <td>Are you currently employed? </td>
+                    <td>
+                        <select name="workexp_current_employed" id="workexp_current_employed" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If you are employed, are you currently working
+                        for your future sponsor?
+                    </td>
+                    <td>
+                        <select name="workexp_current_future_sponsor" id="workexp_current_future_sponsor" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>What is your current or most recent
+                        occupation?
+                    </td>
+                    <td> <input type="text" name="workexp_current_recent_occupation" id="workexp_current_recent_occupation" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Are you retired? </td>
+                    <td>
+                        <select name="workexp_retired" id="workexp_retired" class="form-control">
+                            <option value="selectoption"> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If retired, when did you retire? </td>
+                    <td> <input type="date" name="workexp_if_retired" id="workexp_if_retired" /> </td>
+                </tr>
+
+                <tr>
+                    <td>What was your last occupation before
+                        retiring
+                    </td>
+                    <td> <input type="text" name="workexp_last_occupation" id="workexp_last_occupation" /> </td>
+                </tr>
 
 
-                                    <table>
-                                        <tbody>
-                                            <tr>
-                                                <td> Full name of the school
-                                                </td>
-                                                <td> <input type="text" name="edu_info_junior_fullname" id="edu_info_junior_fullname"  @if(isset($data['education'][0]->edu_info_junior_fullname)) value="{{$data['education'][0]->edu_info_junior_fullname}}" @endif /> </td>
-                                            </tr>
+            </tbody>
+        </table>
 
-                                            <tr>
-                                                <td> Full address
-                                                </td>
-                                                <td> <input type="text" name="edu_info_junior_fulladdress" id="edu_info_junior_fulladdress" @if(isset($data['education'][0]->edu_info_junior_fulladdress)) value="{{$data['education'][0]->edu_info_junior_fulladdress}}" @endif/> </td>
-                                            </tr>
 
-                                            <tr>
-                                                <td> Year obtained </td>
-                                                <td> <input type="date" name="edu_info_junior_yearobtainted" id="edu_info_junior_yearobtainted" @if(isset($data['education'][0]->edu_info_junior_yearobtainted)) value="{{$data['education'][0]->edu_info_junior_yearobtainted}}" @endif/>
-                                                </td>
-                                            </tr>
+        <p style="color:red">FOR EACH WORKING PERIOD PLEASE INDICATE THE FOLLOWING. PLEASE REFER ONLY TO WORK THAT IS RELEVANT
+            TO THE OCCUPATION YOU WILL BE PERFORMING ON YOUR VISA. </p>
 
-                                            <tr>
-                                                <td> Legal duration of the course </td>
-                                                <td> <input type="text" name="edu_info_junior_course" id="edu_info_junior_course" @if(isset($data['education'][0]->edu_info_junior_course)) value="{{$data['education'][0]->edu_info_junior_course}}" @endif/>
-                                                </td>
-                                            </tr>
+        <table>
+            <tbody>
+                <tr>
+                    <td> Your Position
+                    </td>
+                    <td> <input type="text" name="workexp1_your_position" id="workexp1_your_position" /> </td>
+                </tr>
 
-                                            <tr>
-                                                <td> Indicate the START date of your studies (DD-MMYYY)
-                                                </td>
-                                                <td> <input type="date" name="edu_info_junior_startdate" id="edu_info_junior_startdate" @if(isset($data['education'][0]->edu_info_junior_startdate)) value="{{$data['education'][0]->edu_info_junior_startdate}}" @endif/> </td>
-                                            </tr>
+                <tr>
+                    <td>Position Type
+                    </td>
+                    <td>
+                        <select name="workexp1_your_position_type" id="workexp1_your_position_type" class="form-control">
+                            <option value=""> Select Option </option>
+                            <option value="fulltime"> Full Time </option>
+                            <option value="parttime"> Part Time </option>
+                            <option value="other"> Other </option>
+                        </select>
+                    </td>
+                </tr>
 
-                                            <tr>
-                                                <td> Indicate the END date of your studies (DD-MMYYY)
-                                                </td>
-                                                <td> <input type="date" name="edu_info_junior_enddate"
-                                                        id="edu_info_junior_enddate" @if(isset($data['education'][0]->edu_info_junior_enddate)) value="{{$data['education'][0]->edu_info_junior_enddate}}" @endif/>
-                                                </td>
-                                            </tr>
 
-                                        </tbody>
-                                    </table>
+                <tr>
+                    <td>Employer/Name of the business you work for
+                    </td>
+                    <td> <input type="text" name="workexp1_employer_name" id="workexp1_employer_name" /> </td>
+                </tr>
 
-                                  
-                                
-                                
-                               
-                                    </div>
-                                <input type="button" name="next" class="next action-button" value="Next" /> 
-                                <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
-                            </fieldset>
+                <tr>
+                    <td>Country where the business is located
+
+                    </td>
+                    <td> <input type="text" name="workexp1_business_country" id="workexp1_business_country" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Date work started
+
+                    </td>
+                    <td> <input type="date" name="workexp1_date_work_started" id="workexp1_date_work_started" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Date work ended (if still working please leave
+                        blank)
+
+                    </td>
+                    <td> <input type="date" name="workexp1_date_work_ended" id="workexp1_date_work_ended" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Description of duties (please indicate 5 main
+                        duties)
+
+                    </td>
+                    <td> <input type="date" name="workexp1_description_duties" id="workexp1_description_duties" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Please confirm that this work experience is
+                        related to the nominated position
+
+                    </td>
+                    <td> <input type="date" name="workexp1_nominated_position" id="workexp1_nominated_position" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Was this work performed in Australia or New
+                        Zealand?
+                    </td>
+                    <td>
+                        <select name="workexp1_performed_in_aus_nz" id="workexp1_performed_in_aus_nz" class="form-control">
+                            <option value=""> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If yes, please specify with what visa you have
+                        worked with this employer
+
+                    </td>
+                    <td> <input type="date" name="workexp1_visa_employer" id="workexp1_visa_employer" /> </td>
+                </tr>
+
+                <tr>
+                    <td>Did you receive a payslip for this work or an
+                        invoice?
+                    </td>
+                    <td>
+                        <select name="workexp1_payslips_for_this_week" id="workexp1_payslips_for_this_week" class="form-control">
+                            <option value=""> Select Option </option>
+                            <option value="payslip"> Payslip </option>
+                            <option value="invoice"> Invoice </option>
+                            <option value="other"> Other </option>
+
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Can you provide the contact details of a person
+                        for this work period
+                    </td>
+                    <td>
+                        <select name="workexp1_contact_details_work_period" id="workexp1_contact_details_work_period" class="form-control">
+                            <option value=""> Select Option </option>
+                            <option value="yes"> Yes </option>
+                            <option value="no"> No </option>
+
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>If yes, please indicate
+                    </td>
+                    <td> <input type="text" name="workexp1_contact_details_work_period_indicate" id="workexp1_contact_details_work_period_indicate" /> </td>
+
+                </tr>
+
+                <tr>
+                    <td>Name & Surname
+                    </td>
+                    <td> <input type="text" name="workexp1_contact_details_work_period_name" id="workexp1_contact_details_work_period_name" /> </td>
+
+                </tr>
+
+                <tr>
+                    <td>Position
+                    </td>
+                    <td> <input type="text" name="workexp1_contact_details_work_period_position" id="workexp1_contact_details_work_period_position" /> </td>
+
+                </tr>
+
+                <tr>
+                    <td>Email
+                    </td>
+                    <td> <input type="email" name="workexp1_contact_details_work_period_email" id="workexp1_contact_details_work_period_email" /> </td>
+
+                </tr>
+
+                <tr>
+                    <td>Phone contact
+                    </td>
+                    <td> <input type="tel" name="workexp1_contact_details_work_period_phone" id="workexp1_contact_details_work_period_phone" /> </td>
+
+                </tr>
+
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;">WORK EXPERIENCE N.1 – DOCS TO BE UPLOADED
+        </h2>
+        <h2 class="fs-title" style="margin-top: 10px;"> EMPLOYMENT CONTRACT LETTER
+        </h2>
+
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Employment contract or equivalent document
+                        Reference letter </td>
+                    <td> <input type="file" name="workexp1_employment_contract" id="workexp1_employment_contract"></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;"> Payslips</h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> 1 payslip of your current year or most recent
+                        year of experience to be uploaded here
+                    </td>
+                    <td> <input type="file" name="workexp1_current_year_of_experience" id="workexp1_current_year_of_experience"></td>
+                </tr>
+
+                <tr>
+                    <td> 1 payslip for each year previous year of work
+                        experience performed with this employer, to be
+                        uploaded here
+                    </td>
+                    <td> <input type="file" name="workexp1_previous_year_of_experience1" id="workexp1_previous_year_of_experience1"></td>
+                </tr>
+
+                <tr>
+                    <td> 1 payslip for each year previous year of work
+                        experience performed with this employer, to be
+                        uploaded here
+                    </td>
+                    <td> <input type="file" name="workexp1_previous_year_of_experience2" id="workexp1_previous_year_of_experience2"></td>
+                </tr>
+
+                <tr>
+                    <td> 1 payslip for each year previous year of work
+                        experience performed with this employer, to be
+                        uploaded here
+                    </td>
+                    <td> <input type="file" name="workexp1_previous_year_of_experience3" id="workexp1_previous_year_of_experience3"></td>
+                </tr>
+
+
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;"> INCOME TAX RETURN (In Italy CUD) FOR EACH YEAR OF WORK PERFORMED </h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td>Income tax return issued for each year of work
+                        performed to be uploaded here
+                    </td>
+                    <td> <input type="file" name="workexp1_income_tax_return1" id="workexp1_income_tax_return1"></td>
+                </tr>
+
+                <tr>
+                    <td>Income tax return issued for each year of work
+                        performed to be uploaded here
+                    </td>
+                    <td> <input type="file" name="workexp1_income_tax_return2" id="workexp1_income_tax_return2"></td>
+                </tr>
+
+                <tr>
+                    <td>Income tax return issued for each year of work
+                        performed to be uploaded here
+                    </td>
+                    <td> <input type="file" name="workexp1_income_tax_return2" id="workexp1_income_tax_return2"></td>
+                </tr>
+
+
+                <tr>
+                    <td>Any other relevant document to be uploaded
+                        here
+                    </td>
+                    <td> <input type="file" name="workexp1_relevant_doc" id="workexp1_relevant_doc"></td>
+                </tr>
+
+
+
+
+
+            </tbody>
+        </table>
+
+        <h2 class="fs-title" style="margin-top: 10px;"> INPS CHART (Italian employment only) </h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td>Tabella estratto contributivo INPS
+                    </td>
+                    <td> <input type="file" name="workexp1_tabella_estratto" id="workexp1_tabella_estratto"></td>
+                </tr>
+            </tbody>
+        </table>
+
+
+    </div>
+    <input type="button" name="next" class="next action-button" value="Next" />
+    <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
+</fieldset>
