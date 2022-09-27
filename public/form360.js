b4dd99da43d19a45a49e373eafdf4b95a928c975
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-09-27 16:41:10 +0500

    changes

--- public/form360.js
+++ public/form360.js
@@ -40,7 +40,7 @@ jQuery(document).ready(function () {
             //  scrollToTop()
             window.scrollTo({ top: 0, behavior: "smooth" });
 
-            if (next_fs.attr("id") == "fieldsettwelve") {
+            if (next_fs.attr("id") == "fieldsetfourteen") {
                 $("#form360").submit();
             }
         }, 500);
@@ -95,89 +95,89 @@ jQuery(document).ready(function () {
 
     setProgressBar(current);
 
-    jQuery(".next").click(function (e) {
-        current_fs = jQuery(this).parent();
-        next_fs = jQuery(this).parent().next();
+    // jQuery(".next").click(function (e) {
+    //     current_fs = jQuery(this).parent();
+    //     next_fs = jQuery(this).parent().next();
 
-        setTimeout(function () {
-            $(".next").val("Next");
+    //     setTimeout(function () {
+    //         $(".next").val("Next");
 
-            next_fs.show();
-            current_fs.animate(
-                { opacity: 0 },
-                {
-                    step: function (now) {
-                        // for making fielset appear animation
-                        opacity = 1 - now;
+    //         next_fs.show();
+    //         current_fs.animate(
+    //             { opacity: 0 },
+    //             {
+    //                 step: function (now) {
+    //                     // for making fielset appear animation
+    //                     opacity = 1 - now;
 
-                        current_fs.css({
-                            display: "none",
-                            position: "relative",
-                        });
-                        next_fs.css({ opacity: opacity });
-                    },
-                    duration: 500,
-                }
-            );
+    //                     current_fs.css({
+    //                         display: "none",
+    //                         position: "relative",
+    //                     });
+    //                     next_fs.css({ opacity: opacity });
+    //                 },
+    //                 duration: 500,
+    //             }
+    //         );
 
-            jQuery("#progressbar li")
-                .eq(jQuery("fieldset").index(next_fs))
-                .addClass("active");
+    //         jQuery("#progressbar li")
+    //             .eq(jQuery("fieldset").index(next_fs))
+    //             .addClass("active");
 
-            setProgressBar(++current);
-            //  scrollToTop()
-            window.scrollTo({ top: 0, behavior: "smooth" });
+    //         setProgressBar(++current);
+    //         //  scrollToTop()
+    //         window.scrollTo({ top: 0, behavior: "smooth" });
 
-            if (next_fs.attr("id") == "fieldsetseven") {
-                $("#form360").submit();
-            }
-        }, 500);
-    });
+    //         if (next_fs.attr("id") == "fieldsetseven") {
+    //             $("#form360").submit();
+    //         }
+    //     }, 500);
+    // });
 
-    jQuery(".previous").click(function () {
-        current_fs = jQuery(this).parent();
-        previous_fs = jQuery(this).parent().prev();
+    // jQuery(".previous").click(function () {
+    //     current_fs = jQuery(this).parent();
+    //     previous_fs = jQuery(this).parent().prev();
 
-        //Remove class active
-        jQuery("#progressbar li")
-            .eq(jQuery("fieldset").index(current_fs))
-            .removeClass("active");
+    //     //Remove class active
+    //     jQuery("#progressbar li")
+    //         .eq(jQuery("fieldset").index(current_fs))
+    //         .removeClass("active");
 
-        //show the previous fieldset
-        previous_fs.show();
+    //     //show the previous fieldset
+    //     previous_fs.show();
 
-        //hide the current fieldset with style
-        current_fs.animate(
-            { opacity: 0 },
-            {
-                step: function (now) {
-                    // for making fielset appear animation
-                    opacity = 1 - now;
+    //     //hide the current fieldset with style
+    //     current_fs.animate(
+    //         { opacity: 0 },
+    //         {
+    //             step: function (now) {
+    //                 // for making fielset appear animation
+    //                 opacity = 1 - now;
 
-                    current_fs.css({
-                        display: "none",
-                        position: "relative",
-                    });
-                    previous_fs.css({ opacity: opacity });
-                },
-                duration: 500,
-            }
-        );
-        setProgressBar(--current);
-        //  scrollToTop()
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
+    //                 current_fs.css({
+    //                     display: "none",
+    //                     position: "relative",
+    //                 });
+    //                 previous_fs.css({ opacity: opacity });
+    //             },
+    //             duration: 500,
+    //         }
+    //     );
+    //     setProgressBar(--current);
+    //     //  scrollToTop()
+    //     window.scrollTo({ top: 0, behavior: "smooth" });
+    // });
 
-    function setProgressBar(curStep) {
-        var percent = parseFloat(100 / steps) * curStep;
-        percent = percent.toFixed();
-        jQuery(".progress-bar").css("width", percent + "%");
-    }
+    // function setProgressBar(curStep) {
+    //     var percent = parseFloat(100 / steps) * curStep;
+    //     percent = percent.toFixed();
+    //     jQuery(".progress-bar").css("width", percent + "%");
+    // }
 
-    jQuery(window).keydown(function (event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-            return false;
-        }
-    });
+    // jQuery(window).keydown(function (event) {
+    //     if (event.keyCode == 13) {
+    //         event.preventDefault();
+    //         return false;
+    //     }
+    // });
 });
