992233482f8ac80c48a1f4e62e41662e78dbf73a
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-09-27 20:02:19 +0500

    section 4 For Job completed

--- resources/views/backend/forms/employerform/job.blade.php
+++ resources/views/backend/forms/employerform/job.blade.php
@@ -1,39 +1,104 @@
 <fieldset id="fieldsetone">
-                            <div class="form-card">
-                                <div class="row">
-                                    <div class="col-7">
-                                        <h2 class="fs-title">PERSONAL INFO:</h2>
-                                    </div>
-                                    <div class="col-5">
-                                        <h2 class="steps">Step 1 - 4 </h2>
-                                    </div>
-                                </div>
+    <div class="form-card">
+        <div class="row">
+            <div class="col-7">
+                <h2 class="fs-title">- For Job ad n.2 AFTER THE 28 DAY PERIOD please indicate</h2>
+            </div>
+            <div class="col-5">
+                <h2 class="steps">Step 4 - 4 </h2>
+            </div>
+        </div>
 
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td> Name </td>
-                                            <td> <input type="text" name="p_info_name" id="p_info_name" class="required"  @if(isset($data['personal'][0]->p_info_name)) value="{{$data['personal'][0]->p_info_name}}" @endif /> 
-                                            </td>
-                                        </tr>
+        <table>
+            <tbody>
+                <tr>
+                    <td> Number of people who applied for the
+                        position to be filled </td>
+                    <td> <input type="text" name="job_n2_people" id="job_n2_people" />
+                    </td>
+                </tr>
 
-                                        <tr>
-                                            <td> Surname </td>
-                                            <td> <input type="text" name="p_info_surname" id="p_info_surname" @if(isset($data['personal'][0]->p_info_surname)) value="{{$data['personal'][0]->p_info_surname}}" @endif /> </td>
-                                        </tr>
+                <tr>
+                    <td> Number of people who were shortlisted for
+                        the role </td>
+                    <td> <input type="text" name="job_n2_people_shortlisted" id="job_n2_people_shortlisted" /> </td>
+                </tr>
 
-                                        <tr>
-                                            <td> Are you known by any other names (if so specify
-                                                the names and the reaons) </td>
-                                            <td> <input type="text" name="p_info_othername" id="p_info_othername" @if(isset($data['personal'][0]->p_info_othername)) value="{{$data['personal'][0]->p_info_othername}}" @endif/>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>    
-                            </div>      
-                            <input type="button" name="next" class="next action-button" value="Next" />
+                <tr>
+                    <td> Reason why no one was suitable for the role
+                        and therefore you are offering the full-time
+                        position to the visa applicant </td>
+                    <td> <input type="text" name="job_n2_people_suitable" id="job_n2_people_suitable" /> </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2 class="fs-title">For Job ad n.3 please indicate</h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Name of the Job ad platform n.2 </td>
+                    <td> <input type="text" name="job_n3_people" id="job_n3_people" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Exact date when the job ad was put online </td>
+                    <td> <input type="date" name="job_n3_exact_date" id="job_n3_exact_date" /> </td>
+                </tr>
+
+                <tr>
+                    <td> Please confirm this job ad has been online
+                        for at least 28 days or it will be by the time
+                        it expires </td>
+                    <td> <input type="text" name="job_n3_online" id="job_n3_online" /> </td>
+                </tr>
+
+                <tr>
+                    <td> Please indicate the link to the job ad here
+                    </td>
+                    <td> <input type="text" name="job_n3_indicate" id="job_n3_indicate" /> </td>
+                </tr>
+
+                <tr>
+                    <td> Are you able to provide a payment receipt
+                        for this job ad? If yes, please make sure to
+                        attach this document
+                    </td>
+                    <td> <input type="text" name="job_n3_payment" id="job_n3_payment" /> </td>
+                </tr>
+
+
+            </tbody>
+        </table>
+
+        <h2 class="fs-title">For Job ad n.3 AFTER THE 28 DAY PERIOD please indicate</h2>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td> Number of people who applied for the
+                        position to be filled </td>
+                    <td> <input type="text" name="job_n3_after_people" id="job_n3_after_people" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Number of people who were shortlisted for
+                        the role </td>
+                    <td> <input type="text" name="job_n3_after_people_shortlisted" id="job_n3_after_people_shortlisted" /> </td>
+                </tr>
+
+                <tr>
+                    <td> Reason why no one was suitable for the role
+                        and therefore you are offering the full-time
+                        position to the visa applicant </td>
+                    <td> <input type="text" name="job_n3_after_people_suitable" id="job_n3_after_people_suitable" /> </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+    <input type="button" name="next" class="next action-button" value="Next" />
 </fieldset>
-                        
-                        
-                        
-                        
