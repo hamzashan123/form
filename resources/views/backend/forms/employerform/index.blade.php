03159f4f426b833d4c4f469d46b8018ef668d4f9
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2022-10-03 08:01:23 -0700

    updated

--- resources/views/backend/forms/employerform/index.blade.php
+++ resources/views/backend/forms/employerform/index.blade.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="{{asset('countrycode.js')}}"></script>
 @section('content')
 <div class="container-fluid">
