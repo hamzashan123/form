95bc813c05918cb581939858f66b4b9f85251040
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2022-12-23 20:43:34 +0500

    dsa

--- public/employerform.js
+++ public/employerform.js
@@ -1,5 +1,5 @@
-const phoneInputField5 = document.querySelector('#main_point_contact_number');
-const phoneInput5 = window.intlTelInput(phoneInputField5, {
+const phoneInputField10 = document.querySelector('#main_point_contact_number');
+const phoneInput10 = window.intlTelInput(phoneInputField10, {
     initialCountry: 'au',
     utilsScript:
         'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js',
