41d7c2c982451acae8964ca5f2a8e60e2046291f
Faraz Mundia <farazmuhammad1995@gmail.com>
2023-02-03 01:54:40 +0500

    datatable updates

--- resources/views/backend/supervisors/index.blade.php
+++ resources/views/backend/supervisors/index.blade.php
@@ -2,92 +2,88 @@
 
 @section('content')
 
-    <div class="card shadow mb-4">
-        <div class="card-header py-3 d-flex">
-            <h6 class="m-0 font-weight-bold text-primary">
-                Consultants
-            </h6>
-            <div class="ml-auto">
-                @can('create_supervisor')
-                    <a href="{{ route('admin.supervisors.create') }}" class="btn btn-primary">
-                    <span class="icon text-white-50">
-                        <i class="fa fa-plus"></i>
-                    </span>
-                        <span class="text">Add Consultant</span>
-                    </a>
-                @endcan
-            </div>
+<div class="card shadow mb-4">
+    <div class="card-header py-3 d-flex">
+        <h6 class="m-0 font-weight-bold text-primary">
+            Consultants
+        </h6>
+        <div class="ml-auto">
+            @can('create_supervisor')
+            <a href="{{ route('admin.supervisors.create') }}" class="btn btn-primary">
+                <span class="icon text-white-50">
+                    <i class="fa fa-plus"></i>
+                </span>
+                <span class="text">Add Consultant</span>
+            </a>
+            @endcan
         </div>
-        <!-- @include('partials.backend.filter', ['model' => route('admin.supervisors.index')]) -->
+    </div>
+    <!-- @include('partials.backend.filter', ['model' => route('admin.supervisors.index')]) -->
 
-        <div class="table-responsive">
-            <table id="consultantable" class="table table-hover">
-                <thead>
+    <div class="table-responsive">
+        <table id="consultantable" class="table table-hover">
+            <thead>
                 <tr>
-                <th>ID</th>
+                    <th>ID</th>
                     <th>Name</th>
                     <th>Surname</th>
                     <th>Username</th>
-                   
-                    
-                    
+
+
+
                     <th>Status</th>
                     <th>Role</th>
                     <th>Created On</th>
                     <th class="text-center" style="width: 30px;">Action</th>
                 </tr>
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
                 @forelse($supervisors as $supervisor)
-                    <tr>
-                        <td>{{ $supervisor->id }}</td>
-                        <!-- <td>
+                <tr>
+                    <td>{{ $supervisor->id }}</td>
+                    <!-- <td>
                             @if($supervisor->user_image)
                                 <img src="{{ asset('storage/images/users/' . $supervisor->user_image) }}" alt="{{ $supervisor->full_name }}" width="60" height="60">
                             @else
                                 <img src="{{ asset('img/avatar.png') }}" alt="{{ $supervisor->full_name }}" width="60" height="60">
                             @endif
                         </td> -->
-                        <td>
+                    <td>
                         {{ $supervisor->first_name }}
-                            
-                        </td>
-                        
-                        <td>{{ $supervisor->surname }}
-                        </td>
-                        <td>{{ $supervisor->username }}
-                        </td>
-                       
-                        <td>{{ $supervisor->status }}</td>
-                        <td>{{ $supervisor->roles[0]->name }}<br>
-                        <td>{{ $supervisor->created_at }}<br>
-                        <td>
-                            <div class="btn-group btn-group-sm">
-                                <a href="{{ route('admin.supervisors.edit', $supervisor) }}" class="btn btn-sm btn-primary">
-                                    <i class="fa fa-edit"></i>
-                                </a>
-                                <a href="javascript:void(0);"
-                                   onclick="if (confirm('Are you sure to delete this record?'))
-                                       {document.getElementById('delete-tag-{{ $supervisor->id }}').submit();} else {return false;}"
-                                   class="btn btn-sm btn-danger">
-                                    <i class="fa fa-trash"></i>
-                                </a>
-                            </div>
-                            <form action="{{ route('admin.supervisors.destroy', $supervisor) }}"
-                                  method="POST"
-                                  id="delete-tag-{{ $supervisor->id }}" class="d-none">
-                                @csrf
-                                @method('DELETE')
-                            </form>
-                        </td>
-                    </tr>
+
+                    </td>
+
+                    <td>{{ $supervisor->surname }}
+                    </td>
+                    <td>{{ $supervisor->username }}
+                    </td>
+
+                    <td>{{ $supervisor->status }}</td>
+                    <td>{{ $supervisor->roles[0]->name }}<br>
+                    <td>{{ $supervisor->created_at }}<br>
+                    <td>
+                        <div class="btn-group btn-group-sm">
+                            <a href="{{ route('admin.supervisors.edit', $supervisor) }}" class="btn btn-sm btn-primary">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                            <a href="javascript:void(0);" onclick="if (confirm('Are you sure to delete this record?'))
+                                       {document.getElementById('delete-tag-{{ $supervisor->id }}').submit();} else {return false;}" class="btn btn-sm btn-danger">
+                                <i class="fa fa-trash"></i>
+                            </a>
+                        </div>
+                        <form action="{{ route('admin.supervisors.destroy', $supervisor) }}" method="POST" id="delete-tag-{{ $supervisor->id }}" class="d-none">
+                            @csrf
+                            @method('DELETE')
+                        </form>
+                    </td>
+                </tr>
                 @empty
-                    <tr>
-                        <td class="text-center" colspan="6">No Consultant found.</td>
-                    </tr>
+                <tr>
+                    <td class="text-center" colspan="6">No Consultant found.</td>
+                </tr>
                 @endforelse
-                </tbody>
-                <tfoot>
+            </tbody>
+            <tfoot>
                 <tr>
                     <td colspan="6">
                         <div class="float-right">
@@ -95,80 +91,44 @@
                         </div>
                     </td>
                 </tr>
-                </tfoot>
-            </table>
-        </div>
+            </tfoot>
+        </table>
     </div>
-    <script> 
- $(document).ready(function () {
-    // Setup - add a text input to each footer cell
-    $('#consultantable thead tr')
-        .clone(true)
-        .addClass('filters')
-        .appendTo('#consultantable thead');
- 
-    var table = $('#consultantable').DataTable({
-        "bPaginate": false,
-        "bLengthChange": false,
-        "bFilter": true,
-        "bInfo": false,
-        "bAutoWidth": false ,
-        "searching": false,
-        orderCellsTop: true,
-        fixedHeader: true,
-        columnDefs: [
-            { width: 200, targets: 0 }
-        ],
-        initComplete: function () {
-            var api = this.api();
- 
-            // For each column
-            api
-                .columns()
-                .eq(0)
-                .each(function (colIdx) {
+</div>
+<script>
+    $(document).ready(function() {
+        // Setup - add a text input to each footer cell
+        $('#consultantable thead tr').clone(true).addClass('filters').appendTo('#consultantable thead');
+        var table = $('#consultantable').DataTable({
+            orderCellsTop: true,
+            fixedHeader: true,
+            initComplete: function() {
+                var api = this.api();
+                // For each column
+                api.columns().eq(0).each(function(colIdx) {
                     // Set the header cell to contain the input element
-                    var cell = $('.filters th').eq(
-                        $(api.column(colIdx).header()).index()
-                    );
+                    var cell = $('.filters th').eq($(api.column(colIdx).header()).index());
                     var title = $(cell).text();
                     $(cell).html('<input type="text" placeholder="' + title + '" />');
- 
                     // On every keypress in this input
-                    $(
-                        'input',
-                        $('.filters th').eq($(api.column(colIdx).header()).index())
-                    )
+                    $('input', $('.filters th').eq($(api.column(colIdx).header()).index()))
                         .off('keyup change')
-                        .on('change', function (e) {
+                        .on('keyup change', function(e) {
+                            e.stopPropagation();
                             // Get the search value
                             $(this).attr('title', $(this).val());
                             var regexr = '({search})'; //$(this).parents('th').find('select').val();
- 
                             var cursorPosition = this.selectionStart;
                             // Search the column for that value
                             api
                                 .column(colIdx)
-                                .search(
-                                    this.value != ''
-                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                        : '',
-                                    this.value != '',
-                                    this.value == ''
-                                )
+                                .search((this.value != "") ? regexr.replace('{search}', '(((' + this.value + ')))') : "", this.value != "", this.value == "")
                                 .draw();
-                        })
-                        .on('keyup', function (e) {
-                            e.stopPropagation();
- 
-                            $(this).trigger('change');
-                            $(this)
-                                .focus()[0]
-                                .setSelectionRange(cursorPosition, cursorPosition);
+                            $(this).focus()[0].setSelectionRange(cursorPosition, cursorPosition);
                         });
                 });
-        },
+            }
+        });
     });
- });
 </script>
 @endsection
--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -2,41 +2,41 @@
 
 @section('content')
 
-    <div class="card shadow mb-4">
-        <div class="card-header py-3 d-flex">
-            <h6 class="m-0 font-weight-bold text-primary">
+<div class="card shadow mb-4">
+    <div class="card-header py-3 d-flex">
+        <h6 class="m-0 font-weight-bold text-primary">
             @if(str_contains(url()->current(), '/admin/get-clients'))
-                All Clients
+            All Clients
             @elseif(Auth::user()->hasRole('consultant'))
-                    My Clients
+            My Clients
             @else
-                     Clients And Consultants
+            Clients And Consultants
             @endif
-               
-            </h6>
-            <div class="ml-auto">
-                @can('create_user')
-                @if(str_contains(url()->current(), '/admin/get-clients'))
-                    <a href="{{ route('admin.users.create') }}" class="btn btn-primary">
-                   
-                    <span class="icon text-white-50">
-                        <i class="fa fa-plus"></i>
-                    </span>
-                    
-                        
-                        <span class="text">Add Client</span>
-                   
-                       
-                    </a>
-                    @endif
-                @endcan
-            </div>
-        </div>
-       
 
-        <div class="table-responsive">
-            <table id="clientconsultant" class="table table-hover">
-                <thead>
+        </h6>
+        <div class="ml-auto">
+            @can('create_user')
+            @if(str_contains(url()->current(), '/admin/get-clients'))
+            <a href="{{ route('admin.users.create') }}" class="btn btn-primary">
+
+                <span class="icon text-white-50">
+                    <i class="fa fa-plus"></i>
+                </span>
+
+
+                <span class="text">Add Client</span>
+
+
+            </a>
+            @endif
+            @endcan
+        </div>
+    </div>
+
+
+    <div class="table-responsive">
+        <table id="clientconsultant" class="table table-hover">
+            <thead>
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
@@ -45,71 +45,67 @@
                     <th>Matter</th>
                     @if(Auth::user()->hasRole('admin'))<th>Status</th> @endif
                     <th>Role</th>
-                   <th>Application Status</th> 
-                    
+                    <th>Application Status</th>
+
                     <th>Deadline</th>
-                    @if(Auth::user()->hasRole('admin'))   <th>Consultant</th> @endif
-                    
+                    @if(Auth::user()->hasRole('admin')) <th>Consultant</th> @endif
+
                     <th> Assign Forms</th>
                     <th class="text-center" style="width: 30px;">Action</th>
                 </tr>
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
                 @forelse($users as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>
-                            {{ $user->first_name }}
-                        </td>
-                        
-                        <td>{{ $user->surname }}
-                        </td>
-                        <td>{{ $user->username }}
-                        </td>
-                        <td>@if($user->roles[0]->name == 'user') {{$user->matter}} @else  <strong > -- </strong>  @endif</td>
-                        @if(Auth::user()->hasRole('admin')) <td>{{ $user->status }}</td> @endif
-                        <td>@if($user->roles[0]->name == 'user')  <strong> Client </strong> @else <strong>{{$user->roles[0]->name}} </strong> @endif <br> 
-                         <td>{{ $user->application_status }}</td> 
-                         <td>{{ $user->deadline }}</td> 
-                         @if(Auth::user()->hasRole('admin'))  
-                         <td> ABC Consultant</td> 
-                         @endif
-                        
-                        <td>
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>
+                        {{ $user->first_name }}
+                    </td>
+
+                    <td>{{ $user->surname }}
+                    </td>
+                    <td>{{ $user->username }}
+                    </td>
+                    <td>@if($user->roles[0]->name == 'user') {{$user->matter}} @else <strong> -- </strong> @endif</td>
+                    @if(Auth::user()->hasRole('admin')) <td>{{ $user->status }}</td> @endif
+                    <td>@if($user->roles[0]->name == 'user') <strong> Client </strong> @else <strong>{{$user->roles[0]->name}} </strong> @endif <br>
+                    <td>{{ $user->application_status }}</td>
+                    <td>{{ $user->deadline }}</td>
+                    @if(Auth::user()->hasRole('admin'))
+                    <td> ABC Consultant</td>
+                    @endif
+
+                    <td>
                         <a href="{{ route('admin.user.formslist', $user->id) }}" class="btn btn-sm btn-primary">
-                        <i class="fa fa-eye"> View Forms</i>
-                         </a>
-                        </td>
-                        <td>
-                            <div class="btn-group btn-group-sm">
-                               
-                                <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-sm btn-primary">
-                                    <i class="fa fa-edit"></i>
-                                </a>
-                                @if(Auth::user()->hasRole('admin'))
-                                <a href="javascript:void(0);"
-                                   onclick="if (confirm('Are you sure to delete this record?'))
-                                       {document.getElementById('delete-tag-{{ $user->id }}').submit();} else {return false;}"
-                                   class="btn btn-sm btn-danger">
-                                    <i class="fa fa-trash"></i>
-                                </a>
-                                @endif
-                            </div>
-                            <form action="{{ route('admin.users.destroy', $user) }}"
-                                  method="POST"
-                                  id="delete-tag-{{ $user->id }}" class="d-none">
-                                @csrf
-                                @method('DELETE')
-                            </form>
-                        </td>
-                    </tr>
+                            <i class="fa fa-eye"> View Forms</i>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="btn-group btn-group-sm">
+
+                            <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-sm btn-primary">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                            @if(Auth::user()->hasRole('admin'))
+                            <a href="javascript:void(0);" onclick="if (confirm('Are you sure to delete this record?'))
+                                       {document.getElementById('delete-tag-{{ $user->id }}').submit();} else {return false;}" class="btn btn-sm btn-danger">
+                                <i class="fa fa-trash"></i>
+                            </a>
+                            @endif
+                        </div>
+                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST" id="delete-tag-{{ $user->id }}" class="d-none">
+                            @csrf
+                            @method('DELETE')
+                        </form>
+                    </td>
+                </tr>
                 @empty
-                    <tr>
-                        <td class="text-center" colspan="6">No users found.</td>
-                    </tr>
+                <tr>
+                    <td class="text-center" colspan="6">No users found.</td>
+                </tr>
                 @endforelse
-                </tbody>
-                <tfoot>
+            </tbody>
+            <tfoot>
                 <tr>
                     <td colspan="6">
                         <div class="float-right">
@@ -117,81 +113,44 @@
                         </div>
                     </td>
                 </tr>
-                </tfoot>
-            </table>
-        </div>
+            </tfoot>
+        </table>
     </div>
-<script> 
- $(document).ready(function () {
-    // Setup - add a text input to each footer cell
-    $('#clientconsultant thead tr')
-        .clone(true)
-        .addClass('filters')
-        .appendTo('#clientconsultant thead');
- 
-    var table = $('#clientconsultant').DataTable({
-        "bPaginate": false,
-        "bLengthChange": false,
-        "bFilter": true,
-        "bInfo": false,
-        "bAutoWidth": false ,
-        "searching": false,
-        orderCellsTop: true,
-        fixedHeader: true,
-        columnDefs: [
-            { width: 200, targets: 0 }
-        ],
-        initComplete: function () {
-            var api = this.api();
- 
-            // For each column
-            api
-                .columns()
-                .eq(0)
-                .each(function (colIdx) {
+</div>
+<script>
+    $(document).ready(function() {
+        // Setup - add a text input to each footer cell
+        $('#consultantable thead tr').clone(true).addClass('filters').appendTo('#consultantable thead');
+        var table = $('#consultantable').DataTable({
+            orderCellsTop: true,
+            fixedHeader: true,
+            initComplete: function() {
+                var api = this.api();
+                // For each column
+                api.columns().eq(0).each(function(colIdx) {
                     // Set the header cell to contain the input element
-                    var cell = $('.filters th').eq(
-                        $(api.column(colIdx).header()).index()
-                    );
+                    var cell = $('.filters th').eq($(api.column(colIdx).header()).index());
                     var title = $(cell).text();
                     $(cell).html('<input type="text" placeholder="' + title + '" />');
- 
                     // On every keypress in this input
-                    $(
-                        'input',
-                        $('.filters th').eq($(api.column(colIdx).header()).index())
-                    )
+                    $('input', $('.filters th').eq($(api.column(colIdx).header()).index()))
                         .off('keyup change')
-                        .on('change', function (e) {
+                        .on('keyup change', function(e) {
+                            e.stopPropagation();
                             // Get the search value
                             $(this).attr('title', $(this).val());
                             var regexr = '({search})'; //$(this).parents('th').find('select').val();
- 
                             var cursorPosition = this.selectionStart;
                             // Search the column for that value
                             api
                                 .column(colIdx)
-                                .search(
-                                    this.value != ''
-                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                        : '',
-                                    this.value != '',
-                                    this.value == ''
-                                )
+                                .search((this.value != "") ? regexr.replace('{search}', '(((' + this.value + ')))') : "", this.value != "", this.value == "")
                                 .draw();
-                        })
-                        .on('keyup', function (e) {
-                            e.stopPropagation();
- 
-                            $(this).trigger('change');
-                            $(this)
-                                .focus()[0]
-                                .setSelectionRange(cursorPosition, cursorPosition);
+                            $(this).focus()[0].setSelectionRange(cursorPosition, cursorPosition);
                         });
                 });
-        },
+            }
+        });
     });
- });
 </script>
 @endsection
-
