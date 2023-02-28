1d67fbc5b80d423554b123f114f2350b78a909bd
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2023-02-28 16:12:23 +0500

    updated

--- resources/views/backend/forms/form360/workexperiencesectionF.blade.php
+++ resources/views/backend/forms/form360/workexperiencesectionF.blade.php
@@ -24,79 +24,151 @@
                     </td>
                 </tr>
                 <tr>
-                    <td> Name of company in Australia or New Zealand where you will be performing your work 
+                    <td> Name of the Organization where the training will take place 
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
             
                 <tr>
-                    <td> Registration number of the company in Australia or New Zealand (ABN and ACN number  
+                    <td> Website (if available)   
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td> Address of the company in Australia or New Zealand 	 
+                    <td>  (ABN) Business registration ID 	 
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td> Contact number in Australia or New Zealand 
+                    <td>  Address of the organization 
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
+
                 <tr>
-                    <td> Contact person in Australia or New Zealand (name and surname) 
+                    <td> Country 
+                        
+                    </td>
+                    <td>
+                        <select name="fam_member_sectionb_" id="fam_member_sectionb_relationship_partner" class="form-control">
+                            <option value=""> Select Option </option>
+
+                            @if(!empty(isset($data->fam_member_sectionb_relationship_partner)))
+                            <option value="{{$data->fam_member_sectionb_relationship_partner}}" selected>{{$data->fam_member_sectionb_relationship_partner}}</option>
+                            @endif
+                            <option value="AUSTRALIA">AUSTRALIA   </option>
+                            <option value="Newzealand">Newzealand </option>
+                           
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td> Contact surname of the person in the organization 
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                
             
                 <tr>
-                    <td> Role of the contact person in Australia or New Zealand  
+                    <td> Contact name of the person in the organization  
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                
                 
                 <tr>
-                    <td> Company sector in which the company in Australia or NZ operates in
+                    <td> Position of the person in the organization
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td> Position that you will carry out to Australia or New Zealand 
+                    <td> Business phone
+                    </td>
+                    <td> <input type="tel" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
+                </tr>
+                <tr>
+                    <td> Mobile   
+                    </td>
+                    <td> <input type="tel" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
+                </tr>
+                <tr>
+                    <td> Email address  
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td> Main responsibilities and duties to be carried out in Australia or New Zealand  
+                    <td> Position Activity Position details 
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td> Description of the project you will be working during the time in Australia or New Zealand 
+                    <td> Give details of the type of activity the applicant will be undertaking in Australia
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td> Date you are planning to enter Australia or New Zealand 
+                    <td> Your Position as the visa applicant during the training period   
+                    </td>
+                    <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
+                </tr>
+                <tr>
+                    <td> Please indicate the main 5 Duties / activities you will be performing 
+                    </td>
+                    <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
+                </tr>
+                <tr>
+                    <td> Date when the activity will commence 
                     </td>
                     <td> <input type="date" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td>Date you are planning to leave Australia or New Zealand (maximum 6 months stay)  
+                    <td> Date when the activity will end
+                    </td>
+                    <td> <input type="date" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
+                </tr>
+                <tr>
+                    <td> Give details of all street addresses where the activity will be undertaken
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td>Will it be necessary to enter Australia or New Zealand more than one time in order to carry out the job?    
+                    <td> Will you be paid for undertaking the activity in Australia? 
+                        
+                    </td>
+                    <td>
+                        <select name="fam_member_sectionb_" id="fam_member_sectionb_relationship_partner" class="form-control">
+                            <option value=""> Select Option </option>
+
+                            @if(!empty(isset($data->fam_member_sectionb_relationship_partner)))
+                            <option value="{{$data->fam_member_sectionb_relationship_partner}}" selected>{{$data->fam_member_sectionb_relationship_partner}}</option>
+                            @endif
+                            <option value="Yes">Yes </option>
+                            <option value="No"> No </option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td> How much will you be paid for the entire period of stay
                     </td>
                     <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
                 </tr>
                 <tr>
-                    <td>For the work carried out in Australia or New Zealand, will you be paid by the Australian or New Zealand company, or will you receive payments from the company located overseas?  
+                    <td> How frequently will you be paid? 
+                        
                     </td>
-                    <td> <input type="text" name="fam_member_child_name2" id="fam_member_child_name2" @if(isset($data->fam_member_child_name2)) value="{{$data->fam_member_child_name2}}" @endif/> </td>
+                    <td>
+                        <select name="fam_member_sectionb_" id="fam_member_sectionb_relationship_partner" class="form-control">
+                            <option value=""> Select Option </option>
+
+                            @if(!empty(isset($data->fam_member_sectionb_relationship_partner)))
+                            <option value="{{$data->fam_member_sectionb_relationship_partner}}" selected>{{$data->fam_member_sectionb_relationship_partner}}</option>
+                            @endif
+                            <option value="Every week">Every week </option>
+                            <option value="2 weeks"> 2 weeks </option>
+                            <option value="Monthly"> Monthly </option>
+                        </select>
+                    </td>
                 </tr>
                 
 
