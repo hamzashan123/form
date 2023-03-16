5ba8baed9f26c595e7f9699b6c587827667d9fea
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2023-03-16 15:19:17 +0500

    updated

--- resources/views/backend/forms/form360/completeform.blade.php
+++ resources/views/backend/forms/form360/completeform.blade.php
@@ -1,4 +1,11 @@
 ﻿@extends('layouts.admin')
+<style>
+    .container-fluid.completeform360  tr td:nth-child(1) {
+    font-weight: 700;
+    font-size: 15px;
+    color: black !important;
+}
+</style>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="{{asset('formstyles.css')}}">
 
